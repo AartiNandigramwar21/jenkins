6271428a286497d48b5824b9169a2c3a369789dc
Arti Nandigramwar <aartinandigramwar@gmail.com>
2025-11-21 23:20:42 +0530

    file added

---
+++ pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent {Label 'slave'}
+    stages {
+        stage('pull-stage') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo " build sucess"
+            }
+        }
+        stage('test') {
+            steps {
+                echo "test suecss"
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo "deploy sucess"
+            }
+        }
+    }
+}
