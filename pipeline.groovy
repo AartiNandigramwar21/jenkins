0c7fc7ac0a04f44c29e7290c7198df4d80b795ef
Arti Nandigramwar <aartinandigramwar@gmail.com>
2025-11-25 16:52:19 +0530

    added path

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,8 @@ pipeline {
     stages {
         stage('pull-stage') {
             steps {
-                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+                // git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+                git branch: 'main', url: 'https://github.com/Rupen04/artifact_pom.git'
             }
         }
         stage('Build') {
