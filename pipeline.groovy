779dea6cfd360eb5bed5e4805e07bbe461e53fc1
Arti Nandigramwar <aartinandigramwar@gmail.com>
2025-11-27 19:33:57 +0530

    test added again

--- pipeline.groovy
+++ pipeline.groovy
@@ -15,7 +15,7 @@ pipeline {
         stage('test') {
             steps {
                 sh '''mvn sonar:sonar \\
-                -Dsonar.projectKey=projectapp \\
+                -Dsonar.projectKey=token \\
                 -Dsonar.host.url=http://http://13.201.1.164:9000/
                 -Dsonar.login=fcc4363236ee001b774c752bcb4bbd0a05e8a79b'''          
                    }   
