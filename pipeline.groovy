d7b5a792977b417a5564e45752e01bd4d44d6bba
Arti Nandigramwar <aartinandigramwar@gmail.com>
2025-11-29 22:44:50 +0530

    code updated

--- pipeline.groovy
+++ pipeline.groovy
@@ -17,7 +17,7 @@ pipeline {
                   
                sh '''mvn sonar:sonar \\
                   -Dsonar.projectKey=projectapp \\
-                  -Dsonar.host.url=http://3.110.120.203:9000/
+                  -Dsonar.host.url="http://3.110.120.203:9000/"
                   -Dsonar.login=fcc4363236ee001b774c752bcb4bbd0a05e8a79b'''
              }
         }
